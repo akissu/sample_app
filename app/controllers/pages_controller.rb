7bcb2aa8c0b7b4778f60b2d4315d9e7f1af04c0c
Alexander Kissinger <Arexxk@gmail.com>
2011-09-03 22:02:40 -0700

    Added Help page; added base_title

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,27 @@
 class PagesController < ApplicationController
 
+  def baseT
+    return "Ruby on Rails Tutorial Sample App | "
+  end
+
   def home
     @title = "Home"
+    @base_title = baseT
   end
 
   def contact
     @title = "Contact"
+    @base_title = baseT
   end
 
   def about
     @title = "About"
+    @base_title = baseT
+  end
+  
+  def help
+    @title = "Help"
+    @base_title = baseT
   end
 end
 
